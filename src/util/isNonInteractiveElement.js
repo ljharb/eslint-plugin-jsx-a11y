c7269940f0623a023aa6101759a97fabca38ab2c
v1rtl <hi@v1rtl.site>
2024-10-25 16:03:12 +0300

    [Refactor] avoid spreading things that are already arrays
    Co-authored-by: v1rtl <hi@v1rtl.site>
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- src/util/isNonInteractiveElement.js
+++ src/util/isNonInteractiveElement.js
@@ -62,7 +62,7 @@ const nonInteractiveElementRoleSchemas = elementRoleEntries.flatMap(([elementSch
 
 const nonInteractiveAXObjects = new Set(AXObjects.keys().filter((name) => ['window', 'structure'].includes(AXObjects.get(name).type)));
 
-const nonInteractiveElementAXObjectSchemas = [...elementAXObjects].flatMap(([elementSchema, AXObjectsArr]) => (AXObjectsArr.every((role): boolean => nonInteractiveAXObjects.has(role)) ? [elementSchema] : []));
+const nonInteractiveElementAXObjectSchemas = elementAXObjects.flatMap(([elementSchema, AXObjectsArr]) => (AXObjectsArr.every((role): boolean => nonInteractiveAXObjects.has(role)) ? [elementSchema] : []));
 
 function checkIsNonInteractiveElement(tagName, attributes): boolean {
   function elementSchemaMatcher(elementSchema) {
